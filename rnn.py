8722f8dcb7b70735463dfe32cc9017a53aba18ea
Pouya <pouyahallaj@gmail.com>
2022-11-08 12:48:54 +0330

    modified:   rnn.py

--- rnn.py
+++ rnn.py
@@ -5,12 +5,26 @@ import matplotlib.pyplot as plt
 from utils import ALL_LETTERS, N_LETTERS
 from utils import load_data, letter_to_tensor, line_to_tensor, random_training_example
 
+
 class RNN(nn.Module):
     def __init__(self, input_size, hidden_size, output_size):
         super(RNN, self).__init__()
-        
+
         self.hidden_size = hidden_size
         self.i2h = nn.Linear(input_size + hidden_size, hidden_size)
-        self.i2h = nn.Linear(input_size + hidden_size, hidden_size)
+        self.i2o = nn.Linear(input_size + hidden_size, output_size)
+
+        self.softmax = nn.LogSoftmax(dim=1)
         
+    def forward(self, input_tensor, hidden_tensor):
+        combined = torch.cat((input_tensor, hidden_tensor), 1)
         
+        hidden = self.i2h(combined)
+        output = self.i2o(combined)
+        output = self.softmax(output)
+        
+        return output, hidden
+    
+    def init_hidden(self):
+        return torch.zeros(1, self.hidden_size)
+    
