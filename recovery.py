a382425924caaf1d7e228465f39dcd66defb64ac
Pouya <pouyahallaj@gmail.com>
2022-11-10 14:20:43 +0000

    Nov 10, 2022, 5:50 PM

--- recovery.py
+++ recovery.py
@@ -25,9 +25,14 @@ class BasicNN(nn.Module):
         top_relu_output = F.relu(input_to_top_relu)
         scaled_top_relu_output = top_relu_output * self.w01
         
-        input_to_bottom_relu = input*self.w00 +self.b00
+        input_to_bottom_relu = input*self.w10 +self.b10
         bottom_relu_output = F.relu(input_to_bottom_relu)
-        scaled_bottom_relu_output = bottom_relu_output * self.w01
+        scaled_bottom_relu_output = bottom_relu_output * self.w11
         
+        input_to_final_relu = scaled_bottom_relu_output + scaled_top_relu_output + self.final_bias
+        
+        output = F.relu(input_to_final_relu)
+        
+        return output
 
         
