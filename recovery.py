91ad9f3401ce94ba859d06326ab65633bcb01f01
Pouya <pouyahallaj@gmail.com>
2022-11-10 14:09:50 +0000

    Nov 10, 2022, 5:39 PM

--- recovery.py
+++ recovery.py
@@ -6,7 +6,9 @@ from torch.optim import SGD
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+
 class BasicNN(nn.Module):
     def __init__(self) -> None:
         super().__init__()
         self.w00 = nn.Parameter(torch.tensor(1.7), requires_grad=False)
+        
