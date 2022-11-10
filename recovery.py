7b6bd4a90aa09942a9406c7e91a0bc1aac118543
Pouya <pouyahallaj@gmail.com>
2022-11-10 14:13:11 +0000

    Nov 10, 2022, 5:43 PM

--- recovery.py
+++ recovery.py
@@ -12,8 +12,10 @@ class BasicNN(nn.Module):
         super().__init__()
         self.w00 = nn.Parameter(torch.tensor(1.7), requires_grad=False)
         self.b00 = nn.Parameter(torch.tensor(-0.85), requires_grad=False)
-        self.w01 = nn.Parameter(torch.tensor(-0.85), requires_grad=False)
+        self.w01 = nn.Parameter(torch.tensor(-40.8), requires_grad=False)
         
-        self.w10 = nn.Parameter(torch.tensor(-0.85), requires_grad=False)
-        self.b10 = nn.Parameter(torch.tensor(-0.85), requires_grad=False)
-        self.w11 = nn.Parameter(torch.tensor(-0.85), requires_grad=False)
+        self.w10 = nn.Parameter(torch.tensor(12.6), requires_grad=False)
+        self.b10 = nn.Parameter(torch.tensor(0.0), requires_grad=False)
+        self.w11 = nn.Parameter(torch.tensor(2.7), requires_grad=False)
+        
+        self.final_bias = nn.Parameter(torch.tensor(-16))
