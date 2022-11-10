7bbd6a64dfd7fd5ac9f0e84d54b24492e1622692
Pouya <pouyahallaj@gmail.com>
2022-11-10 14:20:56 +0000

    Nov 10, 2022, 5:50 PM

--- recovery.py
+++ recovery.py
@@ -13,26 +13,26 @@ class BasicNN(nn.Module):
         self.w00 = nn.Parameter(torch.tensor(1.7), requires_grad=False)
         self.b00 = nn.Parameter(torch.tensor(-0.85), requires_grad=False)
         self.w01 = nn.Parameter(torch.tensor(-40.8), requires_grad=False)
-        
+
         self.w10 = nn.Parameter(torch.tensor(12.6), requires_grad=False)
         self.b10 = nn.Parameter(torch.tensor(0.0), requires_grad=False)
         self.w11 = nn.Parameter(torch.tensor(2.7), requires_grad=False)
-        
+
         self.final_bias = nn.Parameter(torch.tensor(-16), requires_grad=False)
-        
+
     def forward(self, input):
-        input_to_top_relu = input*self.w00 +self.b00
+        input_to_top_relu = input * self.w00 + self.b00
         top_relu_output = F.relu(input_to_top_relu)
         scaled_top_relu_output = top_relu_output * self.w01
-        
-        input_to_bottom_relu = input*self.w10 +self.b10
+
+        input_to_bottom_relu = input * self.w10 + self.b10
         bottom_relu_output = F.relu(input_to_bottom_relu)
         scaled_bottom_relu_output = bottom_relu_output * self.w11
-        
-        input_to_final_relu = scaled_bottom_relu_output + scaled_top_relu_output + self.final_bias
-        
-        output = F.relu(input_to_final_relu)
-        
-        return output
 
-        
+        input_to_final_relu = (
+            scaled_bottom_relu_output + scaled_top_relu_output + self.final_bias
+        )
+
+        output = F.relu(input_to_final_relu)
+
+        return output
