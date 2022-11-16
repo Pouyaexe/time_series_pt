e72e83470080644d749f4e58b7b4afc495fffcf7
Pouya <pouyahallaj@gmail.com>
2022-11-16 09:00:46 +0000

    Nov 16, 2022, 12:30 PM

--- recovery copy.py
+++ recovery copy.py
@@ -65,7 +65,7 @@ lr_find_result = trainer.tuner.lr_find(
     model,
     dataloader,
     min_lr=0.001,
-    max_lr=1.0,
+    max_lr=10.0,
     early_stop_threshold=None,
 ) # This will run the LR finder to find the best learning rate
 
